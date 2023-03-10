8d7d53e0aa7ed2a37dc22453b5b65857622906b0
Blizzcane <blizzcane@gmail.com>
2023-03-09 20:38:40 -0600

    changed server to just return JSON

--- nba.py
+++ nba.py
@@ -1,27 +1,32 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+
 from nba_api.live.nba.endpoints import scoreboard, boxscore
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/games")
+@app.route("/")
 def get_games():
     board = scoreboard.ScoreBoard()
-    games = board.games.get_dict()
-    games_list = []
-    for game in games:
-        box = boxscore.BoxScore(game['gameId']) 
-        current_score = "{homeScore} - {awayScore}".format(
-            homeScore=box.get_dict()['game']['homeTeam']['score'],
-            awayScore=box.get_dict()['game']['awayTeam']['score'])
-        game_dict = {
-            'gameId': game['gameId'],
-            'awayTeam': game['awayTeam']['teamName'],
-            'homeTeam': game['homeTeam']['teamName'],
-            'currentScore': current_score,
-            'gameStatusText': game['gameStatusText']
-        }
-        games_list.append(game_dict)
-    return jsonify(games_list)
+    return scoreboard.ScoreBoard().get_json()
 
-if __name__ == "__main__":
+    # games = board.games.get_dict()
+    # games_list = []
+    # for game in games:
+    #     box = boxscore.BoxScore(game['gameId'])   
+    #     current_score = "{homeScore} - {awayScore}".format(
+    #         homeScore=box.get_dict()['game']['homeTeam']['score'],
+    #         awayScore=box.get_dict()['game']['awayTeam']['score'])
+    #     game_dict = {
+    #         'gameId': game['gameId'],
+    #         'awayTeam': game['awayTeam']['teamName'],
+    #         'homeTeam': game['homeTeam']['teamName'],
+    #         'currentScore': current_score,
+    #         'gameStatusText': game['gameStatusText']
+    #     }
+    #     games_list.append(game_dict)
+    # return jsonify(games_list)
+
+if __name__ == "__main__": 
     app.run()
