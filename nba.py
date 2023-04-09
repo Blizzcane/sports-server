80ed2e3c1af517347a130e71b82b3f757e2c8e91
Blizzcane <blizzcane@gmail.com>
2023-04-09 17:48:59 -0500

    added waitress

--- nba.py
+++ nba.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+from waitress import serve
 from nba_api.live.nba.endpoints import scoreboard, boxscore
 
 app = Flask(__name__)
@@ -11,22 +11,6 @@ def get_games():
     board = scoreboard.ScoreBoard()
     return scoreboard.ScoreBoard().get_json()
 
-    # games = board.games.get_dict()
-    # games_list = []
-    # for game in games:
-    #     box = boxscore.BoxScore(game['gameId'])   
-    #     current_score = "{homeScore} - {awayScore}".format(
-    #         homeScore=box.get_dict()['game']['homeTeam']['score'],
-    #         awayScore=box.get_dict()['game']['awayTeam']['score'])
-    #     game_dict = {
-    #         'gameId': game['gameId'],
-    #         'awayTeam': game['awayTeam']['teamName'],
-    #         'homeTeam': game['homeTeam']['teamName'],
-    #         'currentScore': current_score,
-    #         'gameStatusText': game['gameStatusText']
-    #     }
-    #     games_list.append(game_dict)
-    # return jsonify(games_list)
 @app.route("/games")
 def get_games_again():
     board = scoreboard.ScoreBoard()
@@ -39,4 +23,4 @@ def get_boxscore():
     return box.get_json()
 
 if __name__ == "__main__": 
-    app.run()
+    serve(app, host='0.0.0.0', port=5000)
